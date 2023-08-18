9b8c31e76d70698f17965527f25772537e462c6d
Th0se <thomasap1464@gmail.com>
2023-08-18 20:27:09 +0700

    remove unused import

--- commercial-site/src/App.js
+++ commercial-site/src/App.js
@@ -1,5 +1,5 @@
 /** @format */
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/About';
